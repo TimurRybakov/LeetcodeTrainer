699881732c33f0e440a1ccee0440ad065960969a
Timur <trybakov@gmail.com>
2024-12-08 02:49:24 +0300

    Added Tests project

---
+++ Tests/AddTwoNumbers.cs
@@ -0,0 +1,50 @@
+namespace Tests;
+
+[TestClass]
+public class AddTwoNumbers
+{
+    [TestMethod]
+    public void Case1()
+    {
+        //Arrange
+        int[] l1 = [2, 4, 3];
+        int[] l2 = [5, 6, 4];
+        int[] expected = [7, 0, 8];
+
+        //Act
+        var result = Problem.AddTwoNumbers(l1, l2).ToArray();
+
+        //Assert
+        CollectionAssert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void Case2()
+    {
+        //Arrange
+        int[] l1 = [0];
+        int[] l2 = [0];
+        int[] expected = [0];
+
+        //Act
+        var result = Problem.AddTwoNumbers(l1, l2).ToArray();
+
+        //Assert
+        CollectionAssert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void Case3()
+    {
+        //Arrange
+        int[] l1 = [9, 9, 9, 9, 9, 9, 9];
+        int[] l2 = [9, 9, 9, 9];
+        int[] expected = [8, 9, 9, 9, 0, 0, 0, 1];
+
+        //Act
+        var result = Problem.AddTwoNumbers(l1, l2).ToArray();
+
+        //Assert
+        CollectionAssert.AreEqual(result, expected);
+    }
+}
